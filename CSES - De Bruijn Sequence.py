2fe93f40ed5da87d1280c2c7251099d85884d529
kekimate <115459634+kekimate@users.noreply.github.com>
2025-11-15 21:15:22 +0100

    Add files via upload

---
+++ CSES - De Bruijn Sequence.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+k = 2
+
+a = [0] * (k * n)
+sequence = []
+
+def db(t, p):
+    if t > n:
+        if n % p == 0:
+            sequence.extend(a[1:p+1])
+    else:
+        a[t] = a[t-p]
+        db(t+1, p)
+        for j in range(a[t-p]+1, k):
+            a[t] = j
+            db(t+1, t)
+
+db(1, 1)
+
+res = "".join(str(x) for x in sequence) + "0" * (n-1)
+print(res)
